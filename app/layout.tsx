cf8b715a67ebe7e5498aeb866742dd62062872b8
romiteld <danny.romitelli@gmail.com>
2025-03-02 07:38:30 -0500

    Add Vercel Analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,5 +15,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return <RootLayoutClient>{children}</RootLayoutClient>
+  return (
+    <>
+      <RootLayoutClient>{children}</RootLayoutClient>
+      <Analytics />
+    </>
+  )
 }
